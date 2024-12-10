35b5c7e98fae383871b08013820e3cd8300c84fd
Demi Nimmo <missdemitg@outlook.com>
2024-12-10 21:37:26 +0000

    updated supabase

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
     reactStrictMode: true,
+    swcMinify: true,
+    env: {
+      NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+      NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+    },
   };
   
