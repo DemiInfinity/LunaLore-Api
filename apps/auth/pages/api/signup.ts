4cd3634572b2690f53c7bf3106b338cdc2ba1ca2
Demi Nimmo <missdemitg@outlook.com>
2024-12-10 22:51:58 +0000

    added cors

--- apps/auth/pages/api/signup.ts
+++ apps/auth/pages/api/signup.ts
@@ -2,6 +2,15 @@ import { supabase } from '../../../../packages/shared-utils/superbase';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
   if (req.method === 'POST') {
     const { email, password } = req.body;
 
