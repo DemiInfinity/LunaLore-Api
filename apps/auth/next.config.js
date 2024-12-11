0910e9d42d51c10ce5787c029739177697f2978d
Demi Nimmo <missdemitg@outlook.com>
2024-12-11 00:38:41 +0000

    Revert "updated next config"
    This reverts commit ae624058424fb1e9bd8ab07c6e9bfeef7d11d085.

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     reactStrictMode: true,
     swcMinify: true,
-    distDir: path.join('..', '..', 'public', 'auth'),
+    distDir: path.join('public'),
     env: {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
