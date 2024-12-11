71379e61c545fac986fd85a97e77f305744a14a0
Demi Nimmo <missdemitg@outlook.com>
2024-12-11 01:56:58 +0000

    updated packages

--- apps/auth/next.config.js
+++ apps/auth/next.config.js
@@ -4,6 +4,10 @@ module.exports = {
     reactStrictMode: true,
     swcMinify: true,
     distDir: path.join('..', '..', 'public', 'auth'),
+    webpack: (config) => {
+      config.resolve.symlinks = false;
+      return config;
+    },
     env: {
       NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
       NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
